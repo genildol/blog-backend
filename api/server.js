25ffaa40f0a0fc65d1fd9e253021d6114752812f
Genildo Lopes <jgmicroshs@gmail.com>
2025-05-25 15:31:31 -0300

    fix: correção de rota com serverless

--- api/server.js
+++ api/server.js
@@ -3,10 +3,10 @@ const serverless = require('serverless-http');
 const fastify = require('fastify')({ logger: true });
 
 fastify.get('/api/posts', async (request, reply) => {
-  const posts = require('./posts.json');
+  const posts = require('../posts.json');
   return { posts };
 });
 
 
-export default serverless(fastiify);
+export default serverless(fastify);
 
